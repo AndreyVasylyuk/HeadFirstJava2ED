06bddd68a01a27eca569265e829a83256013c8e3
AndreyVasylyuk <AndreyVasylyuk@gmail.com>
2021-11-06 23:25:44 +0200

    added DotCom.java DotComBust.java

--- src/io/github/mrkulisav/HeadFirst/Chapter6/DotCom.java
+++ src/io/github/mrkulisav/HeadFirst/Chapter6/DotCom.java
@@ -3,29 +3,34 @@ package io.github.mrkulisav.HeadFirst.Chapter6;
 import java.util.ArrayList;
 
 public class DotCom {
-    private ArrayList<String> locationCells;
-        
-    public void setLocationCells(ArrayList<String> loc) {
-        locationCells = loc;
-    }
-    
-    public String checkYourself(String userInput) {
-        String result = "miss";
-        
-        int index = locationCells.indexOf(userInput);
-        
-        if(index >= 0) {
-            locationCells.remove(index);
-            
-            if(locationCells.isEmpty()) {
-                result = "kill";
-            } else {
-                result = "hit";
-            }
-        }
-        
-        
-        return result;
-    }
+	private ArrayList<String> locationCells;
+	private String name;
+
+	public void setLocationCells(ArrayList<String> loc) {
+		locationCells = loc;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public String checkYourself(String userInput) {
+		String result = "miss";
+
+		int index = locationCells.indexOf(userInput);
+
+		if (index >= 0) {
+			locationCells.remove(index);
+
+			if (locationCells.isEmpty()) {
+				result = "kill";
+				System.out.println("Ouch! You sunk " + name + " :( ");
+			} else {
+				result = "hit";
+			}
+		}
+
+		return result;
+	}
 
 }
--- src/io/github/mrkulisav/HeadFirst/Chapter6/DotComBust.java
+++ src/io/github/mrkulisav/HeadFirst/Chapter6/DotComBust.java
@@ -1,7 +1,71 @@
 package io.github.mrkulisav.HeadFirst.Chapter6;
 
+import java.util.*;
+
 public class DotComBust {
-    public static void main(String[] args) {
-        System.out.println("1");
-    }
+	private GameHelper helper = new GameHelper();
+	private ArrayList<DotCom> dotComsList = new ArrayList<>();
+	private int numOfGuesses = 0;
+
+	private void setUpGame() {
+		DotCom one = new DotCom();
+		one.setName("Yellow.com");
+		DotCom two = new DotCom();
+		two.setName("Red.com");
+		DotCom three = new DotCom();
+		three.setName("Green.com");
+		
+		dotComsList.add(one);
+		dotComsList.add(two);
+		dotComsList.add(three);
+		
+		System.out.println("Your goal is to sink three dot coms.");
+		System.out.println("Try to sink them in the fewest number of guesses.");
+		
+		for (DotCom dotComToSet : dotComsList) {
+			ArrayList<String> newLocation = helper.placeDotCom(3);
+			dotComToSet.setLocationCells(newLocation);
+		}
+	}
+	
+	private void startPlaying() {
+		while(!dotComsList.isEmpty()) {
+			String userGuess = helper.getUserInput("Enter a guess");
+			checkUserGuess(userGuess);
+		}
+		finishGame();
+	}
+	
+	private void checkUserGuess(String userGuess) {
+		numOfGuesses++;
+		String result = "miss";
+		for (DotCom dotComToTest : dotComsList) {
+			result = dotComToTest.checkYourself(userGuess);
+			
+			if (result.equals("hit")) {
+				break;
+			}
+			if (result.equals("kill")) {
+				dotComsList.remove(dotComToTest);
+				break;
+			}
+		}
+		
+		System.out.println(result);
+	}
+	
+	private void finishGame() {
+		System.out.println("All Dot Coms are dead!");
+		if (numOfGuesses <= 18) {
+			System.out.println("It only took you " + numOfGuesses + " guesses.");
+		} else {
+			System.out.println("Took you long enough." + numOfGuesses + "");
+		}
+	}
+
+	public static void main(String[] args) {
+		DotComBust game = new DotComBust();
+		game.setUpGame();
+		game.startPlaying();
+	}
 }
